fc1121db40397750f78a5da8d3796a10c081b3e8
SHREYAS M C <140893678+shreyasmc1234@users.noreply.github.com>
2025-03-07 11:09:35 +0530

    Update access_control.sql

--- access_control.sql
+++ access_control.sql
@@ -23,3 +23,46 @@ must_change_password=true;
 grant role sysadmin to user nishma;
 
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--Logged into to security admin and created the roles and users and granted them the required permissions
+
+-- Logged in to security admin shreyas
+
+CREATE ROLE sales_admin;
+CREATE ROLE sales_users;
+
+grant role sales_users to role sales_admin;
+grant role sales_admin to role sysadmin;
+
+
+create user ramasales password='Password59' default_role=sales_users
+must_change_password=true;
+
+grant role sales_users to user ramasales;
+
+create user yashadmin password='Password59' 
+default_role=sales_admin
+must_change_password=true;
+
+grant role sales_admin to user yashadmin;
+
+=================================================================================================================================================
+
+CREATE ROLE hr_admin;
+CREATE ROLE hr_users;
+
+grant role hr_users to role hr_admin;
+
+
+create user ramahr password='Password59' default_role=hr_users
+must_change_password=true;
+
+grant role hr_users to user ramahr;
+
+create user yashhr password='Password59' 
+default_role=hr_admin
+must_change_password=true;
+
+grant role hr_admin to user yashhr;
+
+
