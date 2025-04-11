7a1478d25fd06796972c376f5748fc9d094c2e1e
SHREYAS M C <140893678+shreyasmc1234@users.noreply.github.com>
2025-04-11 08:16:14 +0530

    Update scd0.sql

--- scd0.sql
+++ scd0.sql
@@ -22,3 +22,36 @@ insert (target.id,target.name,target.age) values(temp.id,temp.name,temp.age)
 
 select * from scd0_stage
 select * from scd0_dim
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--OR
+ --SCD Type 0
+
+ Create or replace transient table stage2(
+ id int,
+ place varchar(100)
+ );
+
+ Create or replace table dim2(
+ id int,
+ place varchar(100)
+ );
+
+ Merge into dim2 as a
+ using stage2 as b
+ on a.id=b.id
+ when not matched then
+ insert (id,place) values(b.id,b.place);
+
+ insert into dim2 values(1,'Mangalore');
+ insert into stage2 values (1,'Sullia');
+ insert into stage2 values (2,'Sullia');
+
+
+ select * from dim2;
+
+ID	PLACE
+1	Mangalore
+2	Sullia
+
+
